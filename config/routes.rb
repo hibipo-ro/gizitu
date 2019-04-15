2cf0685301cd9adfd514c6d5127f55455c78ff22
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-04-15 20:13:54 +0900

    signupページ１

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get "posts/mypage" => "posts#mypage"
-  post "posts/create" => "posts#create"
-  post "posts/:id/update" => "posts#update"
+  get "posts/mypage"        => "posts#mypage"
+  post "posts/create"       => "posts#create"
+  post "posts/:id/update"   => "posts#update"
   # post "posts/:id/destroy" => "posts#destroy"
   post "posts/:id/strength" => "posts#strength"
   post "posts/:id/weakness" => "posts#weakness"
   root to: 'posts#index'
+  get  "/signup"            => "users#new"
   resources :posts
   resources :users
 end
