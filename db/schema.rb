e873ce739b78ce1627a23f9fe1575cb27a00b81d
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-04-29 12:02:24 +0900

    Make a basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_094810) do
+ActiveRecord::Schema.define(version: 2019_04_29_025948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_04_12_094810) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
